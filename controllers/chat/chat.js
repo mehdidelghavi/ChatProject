cd7b3c8eb20b6d34b816ea61d99dc93f813d600e
mehdidelghavi <mehdidelghavideveloper@gmail.com>
2026-05-15 15:44:59 +0330

    Fixing Some Sql

--- controllers/chat/chat.js
+++ controllers/chat/chat.js
@@ -39,7 +39,7 @@ exports.getChat = async (req, res) => {
                             JOIN users u2 ON u2.id = ru2.user_id
                             WHERE ru1.user_id = ?`;
         const [rows2] = await db.execute(conversationSQL, [user.user.id]);
-        const getAdminSQL = "SELECT * from users where role = 'admin'";
+        const getAdminSQL = "SELECT * from users where role = 'Admin' OR role ='Support'";
         const [rows3] = await db.execute(getAdminSQL);
         res.render('index', {
             user: user.user,
